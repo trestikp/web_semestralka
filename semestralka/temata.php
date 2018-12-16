d1a25bffd3455c44aeb798716aff6bee82d44eb8
trestikp <>
2018-12-16 11:55:00 +0100

    rm all

--- semestralka/temata.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "<h3>Vyber si téma</h3>";
--- semestralka/uvod.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "<h3>Úvod --nazev konf.--</h3><p>Popis konference. Nějáké základní informace.</p>";
-echo "<p>Text.</p>";
-echo "<p>Víc textu.</p>";
-echo "<p>Ještě víc textu.</p>";
-echo "<p>Znovu ještě víc textu.</p>";
-echo "<p>A ještě víc textu?!</p>";
-echo "<p>Neuvěřitelně moc textu!</p>";
